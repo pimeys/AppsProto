79502c60add8e006c0c43745acf36d151d20082f
Julius de Bruijn <pimeys@gmail.com>
2010-10-20 16:26:55 +0300

    New product link to index, product creation redirects to root_path, inserted flash notice to product index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -44,7 +44,7 @@ class ProductsController < ApplicationController
 
     respond_to do |format|
       if @product.save
-        format.html { redirect_to(@product, :notice => 'Product was successfully created.') }
+        format.html { redirect_to(root_path, :notice => 'Product was successfully created.') }
         format.xml  { render :xml => @product, :status => :created, :location => @product }
       else
         format.html { render :action => "new" }
